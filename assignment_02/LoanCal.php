be1dd9ef2057bcd636d91d6d4a614177f1f2d2aa
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-17 21:18:38 -0800

    added index file

--- assignment_02/LoanCal.php
+++ assignment_02/LoanCal.php
@@ -8,39 +8,7 @@
  */
 
 ?>
-<html>
-<head>
-    <title>Product Discount Calculator</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
-</head>
 
-<body>
-<h1 align="center"> Loan Calculator </h1>
-<div>
-    <form>
-        <table align="center">
-            <tr>
-                <td> Loan Amount</td>
-                <td><input name="loan_amount" type="text" value="5000"/></td>
-            </tr>
-            <tr>
-                <td> Interest Rate (annual %)</td>
-                <td><input name="annual_rate" type="text" value="12"/></td>
-            </tr>
-            <tr>
-                <td> Period in years</td>
-                <td><input name="years" type="text" value="3"></td>
-            </tr>
-            <tr>
-
-                <td colspan="2">
-                      <input type="submit"> </td>
-            </tr>
-
-        </table>
-    </form>
-
-</div>
 <?php
 //Loan amount
 $loanAmount = $_REQUEST['loan_amount'];
@@ -60,7 +28,4 @@ if ($loanAmount && $annualRate && $periodInYears) { // Check if these values are
 
 }
 ?>
-</body>
-
-</html>
 
---
+++ assignment_02/index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+<h1 align="center"> Loan Calculator </h1>
+<div>
+    <form>
+        <table align="center">
+            <tr>
+                <td> Loan Amount</td>
+                <td><input name="loan_amount" type="text" value="5000"/></td>
+            </tr>
+            <tr>
+                <td> Interest Rate (annual %)</td>
+                <td><input name="annual_rate" type="text" value="12"/></td>
+            </tr>
+            <tr>
+                <td> Period in years</td>
+                <td><input name="years" type="text" value="3"></td>
+            </tr>
+            <tr>
+
+                <td colspan="2">
+                      <input type="submit"> </td>
+            </tr>
+
+        </table>
+    </form>
+
+</div>
+</body>
+</html>
