9852143a18cc9416179f452e2c59e434ddea73f9
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-29 20:57:55 -0800

    moved form to form.php

---
+++ assignment3/form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Paint Estimator</title>
+</head>
+<body>
+<form action="calc.php" method="post">
+	<input type="number" name="sqftOfWallSpace" placeholder="Please enter sqftOfWallSpace to be painted">
+	<input type="number" name="priceOfPaintPerGallon" placeholder="Please enter priceOfPaintPerGallon">
+</form>
+</body>
+</html>
