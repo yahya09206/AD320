6635dab6c74600ea8295cf96e0080509ea78f6a0
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-29 20:57:23 -0800

    fixed input field values

--- assignment3/calc.php
+++ assignment3/calc.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 <form action="calc.php" method="post">
-	
+	<input type="number" name="sqftOfWallSpace" placeholder="Please enter sqftOfWallSpace to be painted">
+	<input type="number" name="priceOfPaintPerGallon" placeholder="Please enter priceOfPaintPerGallon">
 </form>
 </body>
 </html>
