40f9cf39e3733f881a5142c5fc29d7ae92b32d81
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-30 00:08:44 -0800

    made page cleaner

--- assignment3/calc.php
+++ assignment3/calc.php
@@ -3,21 +3,22 @@
 $sqftOfWall = $_POST['sqftOfWallSpace'];
 $costOfPaint = $_POST['priceOfPaintPerGallon'];
 
-
 //validations
-if(is_numeric($sqftOfWall) && is_numeric($costOfPaint)){
+if(isset($sqftOfWall) && isset($costOfPaint)){
 	$numberOfGallonsOfPaint = $wallSpace / 115;
 	$reqHoursOfLabor = $numberOfGallonsOfPaint * 8;
 	$totCostOfPaintJob = $numberOfGallonsOfPaint * $costOfPaint;
 	$totLaborCharges = $reqHoursOfLabor * 20;
 	$totCostOfPaintJob = $totCostOfPaintJob + $totLaborCharges;
 
+}else{
+	echo "Please enter valid values";
+}
+
 	echo "The number of gallons of paint required: " . $numberOfGallonsOfPaint . "<br>";
 	echo "The hours of labor required: " . $numberOfGallonsOfPaint . "<br>";
 	echo "The cost of the paint: " . $numberOfGallonsOfPaint . "<br>";
 	echo "The labor charges: " . $numberOfGallonsOfPaint . "<br>";
 	echo "The total cost of the paint job: " . $numberOfGallonsOfPaint . "<br>";
-
-}
 
 ?>
--- assignment3/form.php
+++ assignment3/form.php
@@ -4,12 +4,49 @@
 	<title>Paint Estimator</title>
 </head>
 <body>
-<form action="calc.php" method="post">
-	<label>Please enter number of square feet of wallspace
-	<input type="text" name="sqftOfWallSpace"></label>
-	<label>please enter price of paint per gallon
-	<input type="text" name="priceOfPaintPerGallon"></label>
-	<input type="submit" name="submit">
-</form>
+<h1 align="center"> Paint Cost Estimator </h1>
+<div>
+    <form>
+        <table align="center">
+            <tr>
+                <td> Amount of wall space in square ft</td>
+                <td><input name="sqftOfWallSpace" type="text"/></td>
+            </tr>
+            <tr>
+                <td> Price of paint/gallon</td>
+                <td><input name="priceOfPaintPerGallon" type="text"/></td>
+            </tr>
+            <tr>
+
+                <td colspan="2">
+                      <input type="submit"> </td>
+            </tr>
+
+        </table>
+    </form>
+
+</div>
+<?php 
+//calculated by user input
+$sqftOfWall = $_POST['sqftOfWallSpace'];
+$costOfPaint = $_POST['priceOfPaintPerGallon'];
+
+//validations
+if(isset($sqftOfWall) && isset($costOfPaint)){
+	$numberOfGallonsOfPaint = $wallSpace / 115;
+	$reqHoursOfLabor = $numberOfGallonsOfPaint * 8;
+	$totCostOfPaintJob = $numberOfGallonsOfPaint * $costOfPaint;
+	$totLaborCharges = $reqHoursOfLabor * 20;
+	$totCostOfPaintJob = $totCostOfPaintJob + $totLaborCharges;
+
+	echo "The number of gallons of paint required: " . $numberOfGallonsOfPaint . "<br/>";
+	echo "The hours of labor required: " . $numberOfGallonsOfPaint . "<br/>";
+	echo "The cost of the paint: " . $numberOfGallonsOfPaint . "<br/>";
+	echo "The labor charges: " . $numberOfGallonsOfPaint . "<br/>";
+	echo "The total cost of the paint job: " . $numberOfGallonsOfPaint . "<br/>";
+}
+
+
+?>
 </body>
 </html>
