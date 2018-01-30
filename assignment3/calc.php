705951f9b568678f1ecf0def4ec9b3d9c365c4cb
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-29 23:19:14 -0800

    set up print statements

--- assignment3/calc.php
+++ assignment3/calc.php
@@ -8,12 +8,21 @@ $reqHoursOfLabor;
 $totLaborCharges;
 $totCostOfPaintJob;
 
+//validations
 if(is_numeric($sqftOfWall) && is_numeric($costOfPaint)){
 	$numberOfGallonsOfPaint = $wallSpace / 115;
 	$reqHoursOfLabor = $numberOfGallonsOfPaint * 8;
 	$totCostOfPaintJob = $numberOfGallonsOfPaint * $costOfPaint;
 	$totLaborCharges = $reqHoursOfLabor * 20;
 	$totCostOfPaintJob = $totCostOfPaintJob + $totLaborCharges;
+
+	echo "The number of gallons of paint required: " . $numberOfGallonsOfPaint;
+	echo "The hours of labor required: " . $numberOfGallonsOfPaint;
+	echo "The cost of the paint: " . $numberOfGallonsOfPaint;
+	echo "The labor charges: " . $numberOfGallonsOfPaint;
+	echo "The total cost of the paint job: " . $numberOfGallonsOfPaint;
+
+
 
 }
 
