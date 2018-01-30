b2b42b7a16edf5b822185b42e7bb722305cbcfbe
Yahya Hussein <yahyahussein91@gmail.com>
2018-01-29 22:45:19 -0800

    changed input types to text

--- assignment3/calc.php
+++ assignment3/calc.php
@@ -1,4 +1,14 @@
 <?php 
+//constant numbers
+$sqftOfWallSpace = 115;
+$hoursOfLabor = 8;
+$chargePerHour = 20;
 
+//calculated by user input
+$numberOfGallonsOfPaint;
+$reqHoursOfLabor;
+$costOfPaint;
+$totLaborCharges;
+$totCostOfPaintJob;
 
 ?>
--- assignment3/form.php
+++ assignment3/form.php
@@ -6,9 +6,9 @@
 <body>
 <form action="calc.php" method="post">
 	<label>Please enter number of square feet of wallspace
-	<input type="number" name="sqftOfWallSpace"></label>
+	<input type="text" name="sqftOfWallSpace"></label>
 	<label>please enter price of paint per gallon
-	<input type="number" name="priceOfPaintPerGallon"></label>
+	<input type="text" name="priceOfPaintPerGallon"></label>
 	<input type="submit" name="submit">
 </form>
 </body>
